69734811ffc218edf1298af901d0c5544642899d
Crysicia <lucienmontmartre@yahoo.fr>
2018-11-20 13:48:40 +0100

    Patch tabs bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,17 +19,7 @@
 //= require_tree .
 
 
-$(document).ready(function() {
-  
-  // ----- Tabs
-  $('#tabs > li').next().hide();
-  $('#tabs > li:first').next().show();
-  $('#tabs > li').click(function() {
-    $('#tabs > li').next().hide();
-    $(this).next().show();
-  });
-  // ----- Tabs [END]
-  
+$(document).ready(function () {
   // ----- Dropdown
   $('#dropdown').hover(function () {
       $('#dropdown-content', this).stop().slideDown('medium');
---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,8 @@
+// ----- Tabs
+  $('#tabs > li').next().hide();
+  $('#tabs > li:first').next().show();
+  $('#tabs > li').click(function () {
+    $('#tabs > li').next().hide();
+    $(this).next().show();
+  });
+  // ----- Tabs [END]
